6a8dbf086ac9ef7cb49509405e948f4ca352e040
(s) Connor Smith 1 <csmith25@PM7-0663-mac.socnet.soc.plymouth.ac.uk>
2018-03-14 18:54:19 +0000

    started work on activities page - make table load and then display an image from database

---
+++ Nursery System/ActivitiesModel.swift
@@ -0,0 +1,13 @@
+//
+//  ActivitiesModel.swift
+//  Nursery System
+//
+//  Created by (s) Connor Smith 1 on 14/03/2018.
+//  Copyright © 2018 (s) Connor Smith 1. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesModel: NSObject {
+
+}
